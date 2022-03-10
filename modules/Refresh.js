06dda5691ad3171027420f510fed3504feadc8f2
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-03-10 18:10:33 +0000

    Make Refresh an async funtion

--- modules/Refresh.js
+++ modules/Refresh.js
@@ -1,9 +1,11 @@
 import GetScore from './GetScore.js';
+import DisplayScores from './DisplayScores.js';
 
 const Refresh = () => {
   const button = document.getElementById('refresh');
-  button.addEventListener('click', () => {
-    GetScore();
+  button.addEventListener('click', async () => {
+    const result = await GetScore();
+    DisplayScores(result);
   });
 };
 
