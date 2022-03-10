f509ea9d36e48fc2ab5beb11a86af997d65e6d48
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-03-10 18:41:49 +0000

    Add .js extension to GetScore import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import './style.css';
 import GetUserInput from '../modules/Form.js';
 import Refresh from '../modules/Refresh.js';
 import DisplayScores from '../modules/DisplayScores.js';
-import GetScore from '../modules/GetScore';
+import GetScore from '../modules/GetScore.js';
 
 const init = async () => {
   const data = await GetScore();
