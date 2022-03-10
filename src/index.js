0aa797b2b44b5bf75914ba6b53b28982e12907b6
Amedzro-Elikplim <amedzroemmanuel@gmail.com>
2022-03-10 18:10:59 +0000

    Fetch and display user score on load

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import './style.css';
 import GetUserInput from '../modules/Form.js';
 import Refresh from '../modules/Refresh.js';
+import DisplayScores from '../modules/DisplayScores.js';
+import GetScore from '../modules/GetScore';
 
+const init = async () => {
+  const data = await GetScore();
+  DisplayScores(data);
+};
+
+init();
 GetUserInput();
 Refresh();
